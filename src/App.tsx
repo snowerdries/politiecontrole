d7aecde5eeea107f60cc7ad09ed76f0e9190e96b
snowerdriesmac <Dries@MacBook-Air.local>
2017-05-31 23:36:24 +0200

    Add material-ui.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import Feed from './feed/feed';
 /// <reference path="../typings/react-tap-event-plugin.d.ts" />
 import injectTapEventPlugin = require('react-tap-event-plugin');
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 
 class App extends React.Component<{}, null> {
   constructor() {    
@@ -12,7 +14,12 @@ class App extends React.Component<{}, null> {
 
   render() {
     return (
-      <Feed />
+      <MuiThemeProvider>
+        <div>
+          <AppBar title="Politiecontrole" showMenuIconButton={false}/>
+          <Feed />
+        </div>        
+      </MuiThemeProvider>
     );
   }
 }
--- src/feed/feed.tsx
+++ src/feed/feed.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import '../App.css';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
 
 export class Feed extends React.Component<{}, null> {
   constructor() {
@@ -9,7 +11,14 @@ export class Feed extends React.Component<{}, null> {
   render() {
     return (
       <div>
-        Dit is de feed       
+        <List>
+          <Subheader>Controles</Subheader>
+          <ListItem primaryText="Controle 1" />
+          <ListItem primaryText="Controle 2" />
+          <ListItem primaryText="Controle 3" />
+          <ListItem primaryText="Controle 4" />
+          <ListItem primaryText="Controle 5" />
+        </List>      
       </div>
     );
   }
