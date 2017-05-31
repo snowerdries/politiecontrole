30aab6abd6f54fe3d4033a7bbc4210270dd754c9
snowerdriesmac <Dries@MacBook-Air.local>
2017-05-31 23:22:59 +0200

    Attempt to add material-ui.

---
+++ src/feed/feed.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import '../App.css';
+
+export class Feed extends React.Component<{}, null> {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        Dit is de feed       
+      </div>
+    );
+  }
+}
+
+export default Feed;
